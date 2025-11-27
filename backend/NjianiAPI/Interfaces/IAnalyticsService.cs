6a87d3af62eaf7491dd965ff5c3bf9427775f8b3
Kreezy09 <keith.mwaniki@students.jkuat.ac.ke>
2025-11-27 19:34:53 +0300

    feat: add GetAnalyticsDashboardAsync method to IAnalyticsService interface

--- backend/NjianiAPI/Interfaces/IAnalyticsService.cs
+++ backend/NjianiAPI/Interfaces/IAnalyticsService.cs
@@ -1,4 +1,5 @@
 public interface IAnalyticsService
 {
     Task<DashboardAnalyticsResponseDto> GetDashboardAnalyticsAsync();
+    Task<AnalyticsDashboardResponseDto> GetAnalyticsDashboardAsync();
 }
